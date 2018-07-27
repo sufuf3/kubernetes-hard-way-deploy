fd6cd0514cb50efd123c6af10e89edb5a66ec932
Samina Fu <sufuf3@gmail.com>
2018-07-27 10:56:31 +0800

    Update e2escript to enable local env before testing

--- test-cluster/e2escript.sh
+++ test-cluster/e2escript.sh
@@ -1,12 +1,12 @@
 mkdir ~/itri-e2e
 
-cd ~/go/src/k8s.io/kubernetes && GINKGO_PARALLEL=y go run hack/e2e.go -- --provider=skeleton --build --up --test --test_args="--ginkgo.focus=\[Conformance\] --ginkgo.skip=\[(Serial|It|Flaky|Slow|sig-scheduling|sig-cli|sig-network|Feature:.*)\]" --dump  --down| tee -a ~/itri-e2e/itri-test.log
-
 cat << EOF >> ~/.bashrc
 alias stripcolors='sed "s/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g"'
 EOF
 
 source ~/.bashrc
+
+cd ~/go/src/k8s.io/kubernetes && GINKGO_PARALLEL=y go run hack/e2e.go -- --provider=skeleton --build --up --test --test_args="--ginkgo.focus=\[Conformance\] --ginkgo.skip=\[(Serial|It|Flaky|Slow|sig-scheduling|sig-cli|sig-network|Feature:.*)\]" --dump  --down| tee -a ~/itri-e2e/itri-test.log
 
 echo -ne "$(cat ~/itri-e2e/itri-test.log)" | stripcolors >> ~/itri-e2e/e2e.log
 
